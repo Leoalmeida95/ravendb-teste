93b1dd298e114f40a7c6eb50f040d520c8237c58
Leonardo Almeida da Silva <leonardo.silva@intranet.cesgranrio.org.br>
2020-01-10 16:29:51 -0300

    Init

---
+++ RavenDBTest/RavenDBTest/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Raven.Client.Documents;
+
+namespace RavenDBTest
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var session = DocumentStoreHolder.Store.OpenSession())
+            {
+                var p = session.Load<Product>("products/9-A");
+                System.Console.WriteLine(p.Name + "" + p.Category);
+                Console.ReadKey();
+            }
+        }
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Supplier { get; set; }
+        public string Category { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public float PricePerUnit { get; set; }
+        public int UnitsInStock { get; set; }
+        public int UnitsOnOrder { get; set; }
+        public bool Discontinued { get; set; }
+        public int ReorderLevel { get; set; }
+    }
+
+    public static class DocumentStoreHolder
+    {
+        private static readonly Lazy<IDocumentStore> LazyStore =
+            new Lazy<IDocumentStore>(() =>
+            {
+                var store = new DocumentStore
+                {
+                    Urls = new[] { "http://live-test.ravendb.net" },
+                    Database = "CesgranrioLeo"
+                };
+
+                return store.Initialize();
+            });
+
+        public static IDocumentStore Store =>
+            LazyStore.Value;
+    }
+}
